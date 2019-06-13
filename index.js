da3cecbf56a34d0b6ebe8719a3a2b0efb9c1b03e
Janette Madera <jemadera24@gmail.com>
2019-06-13 00:33:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 function happyHolidays(){return "Happy holidays!"}
-function happyHolidaysTo(you){return "Happy holidays,${Name}!"}
+function happyHolidaysTo(Name){return "Happy holidays, you!"}
 function happyHolidayTo(Holiday,Name){return "Happy ${Holiday},${Name}!"}
 function holidayCountdown(Holiday,Days){return "It's ${Days} days until ${Holiday}!"}
