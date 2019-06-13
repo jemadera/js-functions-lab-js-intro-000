cac7663937c316ff26f0eba9836133058fe62e9a
Janette Madera <jemadera24@gmail.com>
2019-06-13 00:35:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 function happyHolidays(){return "Happy holidays!"}
 function happyHolidaysTo(Name){return "Happy holidays, you!"}
-function happyHolidayTo(Holiday,Name){return "Happy Independence Day,you!"}
+function happyHolidayTo(Holiday,Name){return "Happy Independence Day, you!"}
 function holidayCountdown(Holiday,Days){return "It's 20 days until Mother's Day!"}
